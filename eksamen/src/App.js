be9f1281e26e29ea8ca667c15137e6100070be1c
kollegard <mathias@kollegard.net>
2023-04-20 14:57:39 +0200

    lagt til fungerende api-kall

--- eksamen/src/App.js
+++ eksamen/src/App.js
@@ -2,22 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  
+  const games = async() =>{
+    const response = await fetch (`https://api.rawg.io/api/platforms?key=a9cc790b6f6e4c0481c8687987fea064`)
+    const data = await response.json()
+    console.log(data)
+  }
+
+games()
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
