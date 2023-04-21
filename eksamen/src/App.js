4c9a103a63d8b3bffcdf139c744baa1553d6adfc
kollegard <mathias@kollegard.net>
2023-04-21 10:49:17 +0200

    lagt til useeffect

--- eksamen/src/App.js
+++ eksamen/src/App.js
@@ -1,16 +1,25 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
+  const [games, setGames] = useState ([])
+
   
-  const games = async() =>{
+  const info = async() =>{
     const response = await fetch (`https://api.rawg.io/api/platforms?key=a9cc790b6f6e4c0481c8687987fea064`)
     const data = await response.json()
-    console.log(data)
+
+    setGames(data.results)
+
+    console.log(games) 
   }
 
-games()
+useEffect(() => {
+  info()
+  }, [])
+  //https://legacy.reactjs.org/docs/hooks-effect.html
 
 
 
---
+++ eksamen/src/components/Gamecard.js
@@ -0,0 +1,3 @@
+export default function Gamecard(){
+    return (<></>)
+}
