b755d6c1e99079362d9a679dbef047a3a14b1a85
kollegard <mathias@kollegard.net>
2023-05-02 12:27:55 +0200

    startpunkt for minstekravet

--- eksamen/src/App.js
+++ eksamen/src/App.js
@@ -1,26 +1,8 @@
-import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
 
-  const [games, setGames] = useState ([])
-
   
-  const info = async() =>{
-    const response = await fetch (`https://api.rawg.io/api/platforms?key=a9cc790b6f6e4c0481c8687987fea064`)
-    const data = await response.json()
-
-    setGames(data.results)
-  // hentet fra Mathias Kollegård sitt arbeidskrav 4
-  }
-
-  console.log(games)
-
-useEffect(() => {
-  info()
-  }, [])
-  //https://legacy.reactjs.org/docs/hooks-effect.html
-
 
 
   return (
