c36804d2e6107a3f0f5ff024704df4c0eaa3f455
Christopherhalv0rsen <chrishp1997@hotmail.com>
2023-05-15 19:41:56 +0200

    ..

--- eksamen/src/components/MyGames.js
+++ eksamen/src/components/MyGames.js
@@ -1,3 +1,22 @@
-export default function MyGames(){
-    return(<></>)
+import React from "react";
+
+const MyGames = ({games}) =>{
+    return (
+        <article>
+            <h2>My Games</h2>
+            {games.map((games) => (
+                <ul key={games.id}>
+                    <img src={games.img} alt={games.title}/>
+                    <h2>{games.title}</h2>
+                    <p>{games.relesed}</p>
+                    <p>{games.genres}</p>
+                    <a href={games.link}>
+                        <button>Kjøp Spill</button>
+                    </a>
+                </ul>
+            ))}
+        </article>
+    )
 }
+
+export default MyGames; 
