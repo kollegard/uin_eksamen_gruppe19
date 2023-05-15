8b3698c8eb4f24ed001d92e9b04b3f3303c3cf9f
Christopherhalv0rsen <chrishp1997@hotmail.com>
2023-05-15 19:31:55 +0200

    Mygames

--- eksamen/src/components/MyGames.js
+++ eksamen/src/components/MyGames.js
@@ -1,4 +1,22 @@
-import {store, mygames} from "./games"
-export default function MyGames(){
-    return(<></>)
+import React from "react";
+
+const MyGames = ({games}) =>{
+    return (
+        <article>
+            <h2>My Games</h2>
+            {games.map((games) => (
+                <ul key={games.id}>
+                    <img src={games.img} alt={games.title}/>
+                    <h2>{games.title}</h2>
+                    <p>{games.relesed}</p>
+                    <p>{games.genres}</p>
+                    <a href={games.link}>
+                        <button>Kjøp Spill</button>
+                    </a>
+                </ul>
+            ))}
+        </article>
+    )
 }
+
+export default MyGames; 
