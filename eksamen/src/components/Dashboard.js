bfba8763a092380e659ef96aaed5024d835ff9c9
Christopherhalv0rsen <chrishp1997@hotmail.com>
2023-05-18 21:12:55 +0200

    ...

--- eksamen/src/components/Dashboard.js
+++ eksamen/src/components/Dashboard.js
@@ -8,14 +8,15 @@ export default function Dashboard(){
     const frontPageMygames = mygames.slice (0,4)
     return(
         <div id="dashboard">
-            <GameShop sliceVal={3}/>
             <NavLink key={"gameshop"} to={"GameShop"}><button id="gameshoplinkbtn">Go to gameshop</button></NavLink>
-
+            <GameShop sliceVal={3}/>
+            
+            
             <MyFavorites sliceVal={2}/>
             <NavLink key={"fav"} to={"MyFavorites"}><button id="favnavlinkbtn">Go to favorites</button></NavLink>
 
             <MyGames games={frontPageMygames}/>
             <NavLink key={"mygames"} to={"MyGames"}><button id="mygameslinkbtn">Go to my games</button></NavLink>
-        </div>
+            </div>
     )
 }
--- eksamen/src/components/GameShop.js
+++ eksamen/src/components/GameShop.js
@@ -6,26 +6,30 @@ export default function GameShop({sliceVal}){
     const [slice,setSlice] = useState(sliceVal)
     
 
-    return(<>
+    return(
 
         <section id="gameShop">
         <h2>Game Shop</h2>
-
-        <ul>
+        <div id="gs">
         {
+            
             store.slice(0,sliceVal).map((games) => (
-                <>
-                <li> <h2>{games.title}</h2></li>
+            
+                <ul>
+                
                 <li> <img src={games.img} alt={`bilde for ${games.title}`}/> </li>
+                <li> <h2>{games.title}</h2></li>
                 <a href={games.link}><button>Buy game</button></a>
                 
-                </>
+                </ul>
+                
             ))
-        }
-        </ul>
+            
+        }</div>
+        
         
         </section>
           
     
-    </>)
+    )
 }
