f9bbf77ad50f43104e23c50fd7b89fecc547db74
oskman <42803141+oskman@users.noreply.github.com>
2023-05-18 23:37:28 +0200

    ..

--- eksamen/src/components/Dashboard.js
+++ eksamen/src/components/Dashboard.js
@@ -4,19 +4,24 @@ import MyGames from "./MyGames";
 import GameShop from "./GameShop";
 import { NavLink } from "react-router-dom";
 
-export default function Dashboard(){
-    const frontPageMygames = mygames.slice (0,4)
-    return(
-        <div id="dashboard">
-            <NavLink key={"gameshop"} to={"GameShop"}><button id="gameshoplinkbtn">Go to gameshop</button></NavLink>
-            <GameShop sliceVal={3}/>
-            
-            
-            <MyFavorites sliceVal={2}/>
-            <NavLink key={"fav"} to={"MyFavorites"}><button id="favnavlinkbtn">Go to favorites</button></NavLink>
+export default function Dashboard() {
+    const frontPageMygames = mygames.slice(0, 4);
 
-            <MyGames games={frontPageMygames}/>
-            <NavLink key={"mygames"} to={"MyGames"}><button id="mygameslinkbtn">Go to my games</button></NavLink>
-            </div>
-    )
-}
+    return (
+      <div id="dashboard">
+
+            <GameShop sliceVal={3}/>
+
+
+
+        <div id="myGamesFavorites">
+          <MyGames games={frontPageMygames} />
+          <MyFavorites sliceVal={2} />
+        </div>
+
+
+
+
+      </div>
+    );
+  }
--- eksamen/src/components/GameShop.js
+++ eksamen/src/components/GameShop.js
@@ -1,35 +1,37 @@
 import {store, mygames} from "./games"
 import React,{useState} from 'react';
+import { NavLink } from "react-router-dom";
 
 
 export default function GameShop({sliceVal}){
     const [slice,setSlice] = useState(sliceVal)
-    
+
 
     return(
 
         <section id="gameShop">
-        <h2>Game Shop</h2>
+        <h2>Game Shop</h2> <NavLink key={"gameshop"} to={"GameShop"}><button id="gameshoplinkbtn">Go to gameshop</button></NavLink>
         <div id="gs">
         {
-            
+
             store.slice(0,sliceVal).map((games) => (
-            
-                <ul>
-                
-                <li> <img src={games.img} alt={`bilde for ${games.title}`}/> </li>
-                <li> <h2>{games.title}</h2></li>
+
+                <ul key={games.id}>
+
+                <li> <img src={games.img} alt={bilde for ${games.title}}/> </li>
+                <h2>{games.title}</h2>
+                <p>Genre: {games.genres}</p>
                 <a href={games.link}><button>Buy game</button></a>
-                
+
                 </ul>
-                
+
             ))
-            
+
         }</div>
-        
-        
+
+
         </section>
-          
-    
+
+
     )
 }
