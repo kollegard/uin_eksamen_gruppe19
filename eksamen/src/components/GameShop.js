914ed93e6d4374661a371bca65ce9838be3bed50
Christopherhalv0rsen <chrishp1997@hotmail.com>
2023-05-18 23:36:22 +0200

    .

--- eksamen/src/components/GameShop.js
+++ eksamen/src/components/GameShop.js
@@ -1,5 +1,6 @@
 import {store, mygames} from "./games"
 import React,{useState} from 'react';
+import { NavLink } from "react-router-dom";
 
 
 export default function GameShop({sliceVal}){
@@ -9,16 +10,18 @@ export default function GameShop({sliceVal}){
     return(
 
         <section id="gameShop">
+        <NavLink key={"gameshop"} to={"GameShop"}><button id="gameshoplinkbtn">Go to gameshop</button></NavLink>
         <h2>Game Shop</h2>
         <div id="gs">
         {
             
             store.slice(0,sliceVal).map((games) => (
             
-                <ul>
+                <ul key={games.id}>
                 
                 <li> <img src={games.img} alt={`bilde for ${games.title}`}/> </li>
-                <li> <h2>{games.title}</h2></li>
+                <h2>{games.title}</h2>
+                <p>Genre: {games.genres}</p>
                 <a href={games.link}><button>Buy game</button></a>
                 
                 </ul>
